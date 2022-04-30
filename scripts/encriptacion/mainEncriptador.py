2a1f23d8b83d899402e6cb6e7723cba24ecf21e0
J. Jesús González Mairena <jiuxej@gmail.com>
2022-04-30 13:35:58 +0200

    Funcion de encriptacion con LGR funcionando 👍

--- scripts/encriptacion/mainEncriptador.py
+++ scripts/encriptacion/mainEncriptador.py
@@ -4,6 +4,9 @@ from tkinter import messagebox as mb
 import cv2
 import encriptador
 
+#Para ver el progreso de encriptacion
+from tqdm import tqdm
+
 root = Tk()
 root.title("Encrypt Image")
 
@@ -13,14 +16,12 @@ def encrypt_img():
 
     try:
         import_filename = fd.askopenfile()
-        imagen = cv2.imread(import_filename.name, 0)
+        imagen = cv2.imread(import_filename.name)
 
         tam_x = 10
         tam_y = 10
 
         grillEncrypt = encriptador.getGrillEncryptor(tam_x, tam_y)
-
-        print(grillEncrypt)
 
         apply_encrypt(imagen, grillEncrypt)
 
@@ -33,7 +34,21 @@ def encrypt_img():
 
 
 def apply_encrypt(img, grill):
-    pass
+
+    center_grill_x = len(grill[0]) // 2
+    center_grill_y = len(grill) // 2
+
+    img_size_x = len(img[0])
+    img_size_y = len(img)
+
+    for i in tqdm(range(img_size_y)):
+        for j in range(img_size_x):
+            for i_grill in range(len(grill)):
+                for j_grill in range(len(grill[0])):
+                   img[i][j][0] += img[(i+i_grill)%img_size_y][(j+j_grill)%img_size_x][0] * grill[i_grill][j_grill]
+                   img[i][j][1] += img[(i+i_grill)%img_size_y][(j+j_grill)%img_size_x][1] * grill[i_grill][j_grill]
+                   img[i][j][2] += img[(i+i_grill)%img_size_y][(j+j_grill)%img_size_x][2] * grill[i_grill][j_grill]
+
 
 
 button1 = Button(root, text="Encriptar imagen", width=20,
